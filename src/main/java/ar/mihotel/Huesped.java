fc22d9fee27d74e4b49e97fe5451122c73909377
AGUSTIN <aguscotta.as@gmail.com>
2025-10-31 18:41:07 -0300

    prueba clases

---
+++ src/main/java/ar/mihotel/Huesped.java
@@ -0,0 +1,16 @@
+package ar.mihotel;
+
+import java.util.Date;
+
+public class Huesped {
+    private int id_persona;
+    private int  nro_documento;
+    private String tipo_documento;
+    private String posicion_iva;
+    private Date fecha_nacimiento;
+    private String ocupacion;
+    private String nacionalidad;
+    private String email;
+    private int cuit;
+
+}
